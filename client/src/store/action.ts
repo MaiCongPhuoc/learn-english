a2c3a639a6cdf066778c8aa8b67e8a0123a3933d
MaiCongPhuoc <maiphuoc244@gmail.com>
2025-04-13 08:56:59 +0700

    add file .env

--- client/src/store/action.ts
+++ client/src/store/action.ts
@@ -1,12 +1,14 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const API_URL = process.env.REACT_APP_URL_SERVER_BACKEND;
+
 // Tạo một async action để gọi API
 export const getVocabulary = createAsyncThunk(
   'vocabulary/getVocabulary',
   async () => {
     // Gọi API và trả về kết quả
-    const response = await axios.get('http://localhost:5000/read-text-file');
+    const response = await axios.get(`${API_URL}read-text-file`);
     return response.data; // Giả sử API trả về một giá trị
   }
 );
@@ -16,7 +18,7 @@ export const saveVocabulary = createAsyncThunk(
   'vocabulary/saveVocabulary',
   async (data: any) => {
     // Gọi API và trả về kết quả
-    const response = await axios.post('http://localhost:5000/write-text-file', {
+    const response = await axios.post(`${API_URL}write-text-file`, {
       data,
     });
     return response.data; // Giả sử API trả về một giá trị
@@ -28,12 +30,9 @@ export const updateVocabulary = createAsyncThunk(
   'vocabulary/updateVocabulary',
   async (data: any) => {
     // Gọi API và trả về kết quả
-    const response = await axios.put(
-      'http://localhost:5000/update-vocabulary',
-      {
-        data,
-      }
-    );
+    const response = await axios.put(`${API_URL}update-vocabulary`, {
+      data,
+    });
     return response.data; // Giả sử API trả về một giá trị
   }
 );
@@ -43,9 +42,7 @@ export const removeVocabulary = createAsyncThunk(
   'vocabulary/removeVocabulary',
   async (key: any) => {
     // Gọi API và trả về kết quả
-    const response = await axios.delete(
-      `http://localhost:5000/delete-vocabulary/${key}`
-    );
+    const response = await axios.delete(`${API_URL}delete-vocabulary/${key}`);
     return response.data; // Giả sử API trả về một giá trị
   }
 );
