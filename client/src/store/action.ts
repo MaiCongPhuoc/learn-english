2b068a41ed7a4551ee16da46b31d306935233cac
MaiCongPhuoc <maiphuoc244@gmail.com>
2025-04-14 19:44:49 +0700

    update loading at pages vocabulary

--- client/src/store/action.ts
+++ client/src/store/action.ts
@@ -7,7 +7,9 @@ export const getVocabulary = createAsyncThunk(
   'vocabulary/getVocabulary',
   async () => {
     // Gọi API và trả về kết quả
-    const response = await axios.get(`${API_URL}read-text-file`);
+    const response = await axios.get(
+      `https://learn-english-xb8j.onrender.com/read-text-file`
+    );
     return response.data; // Giả sử API trả về một giá trị
   }
 );
@@ -17,9 +19,12 @@ export const saveVocabulary = createAsyncThunk(
   'vocabulary/saveVocabulary',
   async (data: any) => {
     // Gọi API và trả về kết quả
-    const response = await axios.post(`${API_URL}write-text-file`, {
-      data,
-    });
+    const response = await axios.post(
+      `https://learn-english-xb8j.onrender.com/write-text-file`,
+      {
+        data,
+      }
+    );
     return response.data; // Giả sử API trả về một giá trị
   }
 );
@@ -29,9 +34,12 @@ export const updateVocabulary = createAsyncThunk(
   'vocabulary/updateVocabulary',
   async (data: any) => {
     // Gọi API và trả về kết quả
-    const response = await axios.put(`${API_URL}update-vocabulary`, {
-      data,
-    });
+    const response = await axios.put(
+      `https://learn-english-xb8j.onrender.com/update-vocabulary`,
+      {
+        data,
+      }
+    );
     return response.data; // Giả sử API trả về một giá trị
   }
 );
@@ -41,7 +49,9 @@ export const removeVocabulary = createAsyncThunk(
   'vocabulary/removeVocabulary',
   async (key: any) => {
     // Gọi API và trả về kết quả
-    const response = await axios.delete(`${API_URL}delete-vocabulary/${key}`);
+    const response = await axios.delete(
+      `https://learn-english-xb8j.onrender.com/delete-vocabulary/${key}`
+    );
     return response.data; // Giả sử API trả về một giá trị
   }
 );
